772368cfcc5640ec6890a23a6010bafcac455f26
judithcoders <judith.coders@gmail.com>
2020-08-24 14:44:04 -0500

    add img

--- config/conexion.js
+++ config/conexion.js
@@ -1,3 +1,3 @@
 let mongoose =  require('mongoose');
-mongoose.connect('mongodb://localhost:27017/crud', { useNewUrlParser: true, useUnifiedTopology: true  }) // cambiar al mongodbcloud
+mongoose.connect('mongodb://localhost:27017/crud', { useNewUrlParser: true, useUnifiedTopology: true  }) 
 module.exports = mongoose;

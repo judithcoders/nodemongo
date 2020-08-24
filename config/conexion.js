4eee19e5bf950ef19e9b24d51e47593eefeec90d
judithcoders <judith.coders@gmail.com>
2020-08-24 13:55:02 -0500

    node mongodb

--- config/conexion.js
+++ config/conexion.js
@@ -1,5 +1,3 @@
 let mongoose =  require('mongoose');
-
-mongoose.connect('mongodb+srv://crud:crud@cluster0.xrmsq.mongodb.net/crud',{ useNewUrlParser: true,  useUnifiedTopology: true });
-//mongoose.connect('mongodb://localhost:27017/crud', { useNewUrlParser: true, useUnifiedTopology: true  })
+mongoose.connect('mongodb://localhost:27017/crud', { useNewUrlParser: true, useUnifiedTopology: true  })
 module.exports = mongoose;
